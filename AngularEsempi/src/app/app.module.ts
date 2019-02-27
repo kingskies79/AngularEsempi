899eab6d0986306fca11b9240db78fcfdcf8262d
fabio leotta <fabioleotta@yahoo.it>
2019-02-27 18:19:46 +0100

    Model and template form driven

--- AngularEsempi/src/app/app.module.ts
+++ AngularEsempi/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ModelDriveFormComponent } from './model-drive-form/model-drive-form.component';
+import { FormBuilder, FormsModule, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelDriveFormComponent,
+    TemplateDrivenFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularEsempi/src/test.ts
+++ AngularEsempi/src/test.ts
@@ -1,20 +1,43 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+import {FormBuilder, FormGroup, FormControl, Validator, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
-import 'zone.js/dist/zone-testing';
-import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+@Component({
+  selector: 'test-app',
+  template: 'test.html'
+})
 
-declare const require: any;
+class ModelFormComponent implements OnInit {
+  myform: FormGroup;
+  firstName: FormControl;
+  lastName: FormControl;
+  email: FormControl;
+  password: FormControl;
+  language: FormControl;
 
-// First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
-);
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
+  
+  ngOnInit(){
+    this.createFormControl();
+    this.createForm();
+
+  }
+  createFormControl() {
+    this.firstName = new FormControl('', Validators.required);
+    this.lastName = new FormControl('', Validators.required);
+    this.email = new FormControl('', [Validators.required, Validators.pattern('[^ @]*@[^ @]*')]);
+    this.password = new FormControl('', [Validators.required, Validators.minLength(8)]);
+    this.language = new FormControl('', Validators.required);
+  }
+  createForm() {
+    this.myform = new FormGroup({
+      name: new FormGroup({
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+        password: this.password,
+        language: this.language
+      })
+    });
+
+  }
+}
+
