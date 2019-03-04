d41f8051c6e4389cd7c40ffbac1d97d410875649
fabio leotta <fabioleotta@yahoo.it>
2019-03-04 16:44:37 +0100

    iTunesAppV2

--- AngularEsempi/src/app/app.component.ts
+++ AngularEsempi/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 
-import { ItunesService } from './itunes.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private loading = false;
-  constructor(private itunes: ItunesService) {   }
 
-  doSearch(term: string) {     this.loading = true;     this.itunes.search(term).then(() => this.loading = false)   } 
+  constructor() {   }
 }
