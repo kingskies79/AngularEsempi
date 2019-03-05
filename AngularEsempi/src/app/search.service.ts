1d2cbd00d0eaec74a6893e3a2bacae88f28bf5c9
fabio leotta <fabioleotta@yahoo.it>
2019-03-05 20:34:39 +0100

    commit

---
+++ AngularEsempi/src/app/search.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Jsonp } from '@angular/http';
+import { SearchItem } from './search-item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  itemList: SearchItem[];
+  apiUrl = 'https://itunes.apple.com/search';
+  loading: boolean;
+  constructor(private jsonp: Jsonp) {
+    this.loading = false;
+    this.itemList = [];
+  }
+  doSearch(term: string) {
+    return new Promise((resolve, reject) => {
+      this.jsonp.request(`${this.apiUrl}?term=${term}&media=music&limit=20&callback=JSONP_CALLBACK`)
+        .toPromise()
+        .then(res => {
+          this.itemList = res.json().results.map(item => {
+            return new SearchItem(
+              item.trackName,
+              item.artistName,
+              item.trackViewUrl,
+              item.artworkUrl30,
+              item.artistId
+            );
+          });
+
+          resolve(this.itemList);
+        },
+          msg => {
+            reject(msg);
+          });
+
+    });
+  }
+}
