95cabbbcd955e3b4c4ea97f8209c33859654d0c8
fabio leotta <fabioleotta@yahoo.it>
2019-03-13 08:25:38 +0100

    commit

--- AngularEsempi/src/app/app.module.ts
+++ AngularEsempi/src/app/app.module.ts
@@ -2,19 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {  FormsModule,    } from '@angular/forms';
-import { JokeComponent } from './joke/joke.component';
-import { JokeListComponent } from './joke-list/joke-list.component';
-import { JokeFormComponent } from './joke-form/joke-form.component';
+
 
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    JokeComponent,
-    JokeListComponent,
-    JokeFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- AngularEsempi/src/app/joke.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Joke } from './joke';
-
-describe('Joke', () => {
-  it('should create an instance', () => {
-    expect(new Joke()).toBeTruthy();
-  });
-});
